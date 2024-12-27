cafc0938d323ae86d8e3ee8b785a563a2165e8af
demo <mohammedfayazfn173148@gmail.com>
2024-12-27 21:04:32 +0530

    express new comment

---
+++ app.js
@@ -0,0 +1,38 @@
+// import express
+const express = require("express");
+const logger = require("./utils/logger");
+const errorRoute = require("./utils/errorRoute");
+//const morgan = require("morgan");
+
+// create an express app
+const app = express();
+
+// use the logger middleware
+app.use(logger);
+// app.use(morgan("dev"));
+
+app.use(express.json());
+app.get("/", (request, response) => {
+  response.json({ message: "GET" });
+});
+
+app.post("/", (request, response) => {
+  response.json({ message: "POST" });
+});
+
+app.patch("/", (request, response) => {
+    response.json({ message: "PATCH" });
+});
+
+app.put("/", (request, response) => {
+  response.json({ message: "PUT" });
+});
+
+app.delete("/", (request, response) => {
+  response.json({ message: "DELETE" });
+});
+
+app.use(errorRoute);
+
+// export the app
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+
+// listen for requests and start the server
+app.listen(3000, () => {
+  console.log(`Server is running @ http://127.0.0.1:3000`);
+});
